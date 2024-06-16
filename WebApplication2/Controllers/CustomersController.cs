0f98c66b7846071c1af6a45689c1004fb2d888ba
SmailDeeb <hoakm.net@gmail.com>
2024-06-16 05:46:48 +0300

    .

--- WebApplication2/Controllers/CustomersController.cs
+++ WebApplication2/Controllers/CustomersController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Data;
 using WebApplication2.Models;
@@ -37,9 +32,9 @@ namespace WebApplication2.Controllers
             if (customer == null)
             {
                 return NotFound();
+            }else{
+            return Ok(customer);
             }
-
-            return customer;
         }
 
         // PUT: api/Customers/5
